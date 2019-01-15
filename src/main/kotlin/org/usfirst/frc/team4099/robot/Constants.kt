ee28428d748cd26032d14aea10e466b3a1ed975e
jchen319 <juliachen@Julias-MacBook-Pro.local>
2019-01-15 16:31:04 -0500

    Set elevator constants

--- src/main/kotlin/org/usfirst/frc/team4099/robot/Constants.kt
+++ src/main/kotlin/org/usfirst/frc/team4099/robot/Constants.kt
@@ -1,6 +1,21 @@
 package org.usfirst.frc.team4099.robot
 
 class Constants {
+    object Gains {
+        //subject to change
+        val ELEVATOR_UP_KP = 0.6
+        val ELEVATOR_UP_KI = 0.0008
+        val ELEVATOR_UP_KD = 55.000
+        val ELEVATOR_UP_KF = 0.5700
 
+        val ELEVATOR_DOWN_KP = 1.00
+        val ELEVATOR_DOWN_KI = 0.002
+        val ELEVATOR_DOWN_KD = 60.0
+        val ELEVATOR_DOWN_KF = 0.78
+    }
+
+    object Elevator {
+        val ELEVATOR_TALON_ID = 0
+    }
 
 }
