70c6689a6a1f81e277ceed79eb1118dd0ab1ef42
azhong2002 <azhong2002@outlook.com>
2019-01-19 16:07:21 -0500

    Merge Seek and Aim in Vision.kt

--- src/main/kotlin/org/usfirst/frc/team4099/robot/subsystems/Vision.kt
+++ src/main/kotlin/org/usfirst/frc/team4099/robot/subsystems/Vision.kt
@@ -5,38 +5,40 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import org.usfirst.frc.team4099.robot.loops.Loop
 
-class Aim private constructor(): Subsystem {
+class Vision private constructor(): Subsystem {
     var Kp = -0.1
     var min_command = 0.05
     var steering_adjust = 0.0
 
     private val table: NetworkTable = NetworkTableInstance.getDefault().getTable("limelight")
     var tx = table.getEntry("tx").getDouble(0.0)
+    var tv = table.getEntry("tv").getDouble(0.0)
     val heading_error = (tx * -1.0)
 
-    var aimState = AimState.INACTIVE
+    var visionState = VisionState.INACTIVE
 
-    enum class AimState {
-        AIMING, INACTIVE
+    enum class VisionState {
+        AIMING, INACTIVE, SEEKING
     }
 
     override fun outputToSmartDashboard() {
-        SmartDashboard.putNumber("LimelightX", tx);
+        SmartDashboard.putNumber("LimelightX", tx)
+        SmartDashboard.putNumber("LimelightTarget", tv)
     }
 
     @Synchronized override fun stop() {
-        aimState = AimState.INACTIVE
+        visionState = VisionState.INACTIVE
     }
 
     val loop: Loop = object : Loop {
         override fun onStart() {
-            aimState = AimState.INACTIVE
+            visionState = VisionState.INACTIVE
         }
 
         override fun onLoop() {
-            synchronized(this@Aim) {
-                when (aimState) {
-                    AimState.AIMING -> {
+            synchronized(this@Vision) {
+                when (visionState) {
+                    VisionState.AIMING -> {
                         if (tx > 1.0) {
                             steering_adjust = Kp * heading_error - min_command
                         } else if (tx < 1.0) {
@@ -44,7 +46,15 @@ class Aim private constructor(): Subsystem {
                         }
 
                     }
-                    AimState.INACTIVE -> steering_adjust = 0.0
+                    VisionState.SEEKING -> {
+                        if (tv == 0.0) {
+                            steering_adjust = 0.3
+                        } else {
+                            val heading_error = tx
+                            steering_adjust = Kp * tx
+                        }
+                    }
+                    VisionState.INACTIVE -> steering_adjust = 0.0
                 }
             }
         }
@@ -53,7 +63,7 @@ class Aim private constructor(): Subsystem {
     }
 
     companion object {
-        val instance = Aim()
+        val instance = Vision()
     }
 
     override fun zeroSensors() { }
