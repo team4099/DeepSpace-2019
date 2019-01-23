9be2db7fbd4d0444b4cb09ab9b633421f76e0092
rbhog <rbhogavilli@gmail.com>
2019-01-23 17:50:03 -0500

    Add constants to Constants.kt

--- src/main/kotlin/org/usfirst/frc/team4099/robot/Constants.kt
+++ src/main/kotlin/org/usfirst/frc/team4099/robot/Constants.kt
@@ -1,6 +1,23 @@
 package org.usfirst.frc.team4099.robot
 
 class Constants {
+    object Drive {
+        val LEFT_MASTER_ID = 4
+        val LEFT_SLAVE_1_ID = 5
+        val LEFT_SLAVE_2_ID = 6
+        val RIGHT_MASTER_ID = 8
+        val RIGHT_SLAVE_1_ID = 9
+        val RIGHT_SLAVE_2_ID = 10
+
+        val HIGH_GEAR_MAX_SETPOINT = 17.0  //17 fps
+
+        val SHIFTER_FORWARD_ID = 0
+        val SHIFTER_REVERSE_ID = 1
+    }
+
+    object Wheels {
+        val DRIVE_WHEEL_DIAMETER_INCHES = 0
+    }
 
     object Climber {
         val CLIMBER_F1_FORWARD_ID = 0
@@ -13,6 +30,28 @@ class Constants {
         val CLIMBER_B2_REVERSE_ID = 7
     }
     object Gains {
+        val LEFT_LOW_KP = 0.0000
+        val LEFT_LOW_KI = 0.0000
+        val LEFT_LOW_KD = 0.0000
+        val LEFT_LOW_KF = 1023.0 / 2220.0
+
+        //subject to change
+        val LEFT_HIGH_KP = .1 * 1023 / 70
+        val LEFT_HIGH_KI = 0.0000
+        val LEFT_HIGH_KD = 0.0000
+        val LEFT_HIGH_KF = 1023.0 / 4420.0
+
+        val RIGHT_LOW_KP = 0.0000
+        val RIGHT_LOW_KI = 0.0000
+        val RIGHT_LOW_KD = 0.0000
+        val RIGHT_LOW_KF = 1023.0 / 2220.0
+
+        //subject to change
+        val RIGHT_HIGH_KP = .1 * 1023 / 70
+        val RIGHT_HIGH_KI = 0.0000
+        val RIGHT_HIGH_KD = 0.0000
+        val RIGHT_HIGH_KF = 1023.0 / 4420.0
+
         //subject to change
         val ELEVATOR_UP_KP = 0.6
         val ELEVATOR_UP_KI = 0.0008
@@ -39,6 +78,20 @@ class Constants {
     object Joysticks {
         val DRIVER_PORT = 0
         val SHOTGUN_PORT = 1
+    }
+
+    object Velocity {
+        val HIGH_GEAR_VELOCITY_CONTROL_SLOT = 0
+        val LOW_GEAR_VELOCITY_CONTROL_SLOT = 0
+        val DRIVE_HIGH_GEAR_NOMINAL_OUTPUT = 0.0 //percentage
+        val DRIVE_LOW_GEAR_NOMINAL_OUTPUT = 0.0 //percentage
+        val DRIVE_HIGH_GEAR_MAX_FORWARD_OUTPUT = 1.0 //percentage
+        val DRIVE_HIGH_GEAR_MAX_REVERSE_OUTPUT = -1.0
+        val DRIVE_LOW_GEAR_MAX_FORWARD_OUTPUT = 1.0
+        val DRIVE_LOW_GEAR_MAX_REVERSE_OUTPUT = -1.0
+
+        val SHIFTER_CHANNEL = 0
+        val SHIFTER_MODULE = 0
     }
 
 }
