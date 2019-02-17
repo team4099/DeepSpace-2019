d953da097c1dc4f40f4b83424c7dca687d435b5d
ktorsh <gtorshizi@gmail.com>
2019-02-17 15:42:34 -0500

    Add extend hatch panel action

--- src/main/kotlin/org/usfirst/frc/team4099/auto/actions/HatchPanelExtendAction.kt
+++ src/main/kotlin/org/usfirst/frc/team4099/auto/actions/HatchPanelExtendAction.kt
@@ -5,15 +5,16 @@ import org.usfirst.frc.team4099.robot.subsystems.Intake
 class HatchPanelExtendAction: Action {
     private val mIntake = Intake.instance
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         mIntake.extended
     }
 
     override fun done() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun update() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+    override fun isFinished(): Boolean {
+        return true
     }
 }
